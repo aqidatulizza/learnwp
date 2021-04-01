1810b9340eeca17fc302ccb3964c59166d84b223
Aqidatul Izza <aqiza18@gmail.com>
2021-04-01 16:01:54 +0700

    permalink

--- template-parts/content-image.php
+++ template-parts/content-image.php
@@ -1,6 +1,7 @@
  <article <?php post_class(); ?>> 
-	<h2><?php the_title(); ?></h2>
-	<?php the_post_thumbnail( array( 250, 250 ) ); ?>
+	<a href="<?php the_permalink(); ?>" style="text-decoration: none;"><h2><?php the_title(); ?></h2></a>
+	<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( array( 250, 250 ) ); ?></a>
+
 	<div class="meta-info">
 		<p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
 		<p>Categorie: <?php the_category( ' ' ); ?></p>
